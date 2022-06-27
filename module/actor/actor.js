5ae508882b0df42a0f33a99874a8ff2e217c5b98
yoshisman8 <4214215+yoshisman8@users.noreply.github.com>
2022-06-27 11:43:20 -0400

    Saved Rolls API update
    Some crucial data under the Actor document was not being set up until after the user opens the sheet for the first time. This is bad.
    
    I have mirrored the code from actor-sheet.js into actor.js so that these fields are initialized when the actor is initialized as opposed to only when the sheet is initialized.
    
    Additionally, two new methods have been added to the Actor object:
    • actor.savedRoll() which rolls a saved roll from the actor.
    • actor.getSavedRoll() which returns the RollForm of the saved roll in case the user wants to make last minute edits.

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -1,3 +1,5 @@
+import { RollForm } from "../apps/dice-roller.js";
+
 /**
  * Extend the base Actor entity by defining a custom roll data structure which is ideal for the Simple system.
  * @extends {Actor}
@@ -10,10 +12,9 @@ export class ExaltedThirdActor extends Actor {
   prepareData() {
     super.prepareData();
 
-    const actorData = this.data;
     // Make separate methods for each Actor type (character, npc, etc.) to keep
     // things organized.
-    this._prepareCharacterData(actorData);
+    this._prepareCharacterData();
   }
 
   async displayEmbeddedItem(itemId) {
@@ -127,12 +128,34 @@ export class ExaltedThirdActor extends Actor {
     this.update(actorData);
   }
 
+  async rollSaved(name){
+
+    const roll = Object.values(this.data.data.savedRolls).find(x=>x. name === name);
+
+    if(!roll){
+      return ui.notifications.error(`${this.name} does not have a saved roll named ${name}!`);
+    }
+
+
+    await new RollForm(this, { event: this.event }, {}, { rollId: roll.id, skipDialog: true }).roll();
+  }
+
+  getSavedRoll(name){
+    const roll = Object.values(this.data.data.savedRolls).find(x=>x. name === name);
+
+    if(!roll){
+      return ui.notifications.error(`${this.name} does not have a saved roll named ${name}!`);
+    }
+
+    return new RollForm(this, { event: this.event }, {}, { rollId: roll.id });
+  }
   /**
    * Prepare Character type specific data
    */
-  _prepareCharacterData(actorData) {
+  _prepareCharacterData() {
     // Make modifications to data here. For example:
-    const data = actorData.data;
+
+    const actorData = this;
     // this._prepareBaseActorData(data);
     let totalHealth = 0;
     let currentPenalty = 0;
@@ -141,73 +164,208 @@ export class ExaltedThirdActor extends Actor {
     let currentWarstriderPenalty = 0;
     let currentShipPenalty = 0;
 
-    if (data.battlegroup) {
-      totalHealth = data.health.levels.zero.value + data.size.value;
-      data.health.total = totalHealth;
-      if ((data.health.bashing + data.health.lethal + data.health.aggravated) > data.health.total) {
-        data.health.aggravated = data.health.total - data.health.lethal;
-        if (data.health.aggravated <= 0) {
-          data.health.aggravated = 0
-          data.health.lethal = data.health.total
+    if (actorData.data.data.battlegroup) {
+      totalHealth = actorData.data.data.health.levels.zero.value + actorData.data.data.size.value;
+      actorData.data.data.health.total = totalHealth;
+      if ((actorData.data.data.health.bashing + actorData.data.data.health.lethal + actorData.data.data.health.aggravated) > actorData.data.data.health.total) {
+        actorData.data.data.health.aggravated = actorData.data.data.health.total - actorData.data.data.health.lethal;
+        if (actorData.data.data.health.aggravated <= 0) {
+          actorData.data.data.health.aggravated = 0
+          actorData.data.data.health.lethal = actorData.data.data.health.total
         }
       }
-      data.health.penalty = 0;
+      actorData.data.data.health.penalty = 0;
     }
     else {
-      for (let [key, health_level] of Object.entries(data.health.levels)) {
-        if ((data.health.bashing + data.health.lethal + data.health.aggravated) > totalHealth) {
+      for (let [key, health_level] of Object.entries(actorData.data.data.health.levels)) {
+        if ((actorData.data.data.health.bashing + actorData.data.data.health.lethal + actorData.data.data.health.aggravated) > totalHealth) {
           currentPenalty = health_level.penalty;
         }
         totalHealth += health_level.value;
       }
-      data.health.total = totalHealth;
-      if ((data.health.bashing + data.health.lethal + data.health.aggravated) > data.health.total) {
-        data.health.aggravated = data.health.total - data.health.lethal;
-        if (data.health.aggravated <= 0) {
-          data.health.aggravated = 0;
-          data.health.lethal = data.health.total;
+      actorData.data.data.health.total = totalHealth;
+      if ((actorData.data.data.health.bashing + actorData.data.data.health.lethal + actorData.data.data.health.aggravated) > actorData.data.data.health.total) {
+        actorData.data.data.health.aggravated = actorData.data.data.health.total - actorData.data.data.health.lethal;
+        if (actorData.data.data.health.aggravated <= 0) {
+          actorData.data.data.health.aggravated = 0;
+          actorData.data.data.health.lethal = actorData.data.data.health.total;
         }
       }
-      data.health.penalty = currentPenalty;
+      actorData.data.data.health.penalty = currentPenalty;
     }
 
 
-    for (let [key, health_level] of Object.entries(data.warstrider.health.levels)) {
-      if ((data.warstrider.health.bashing + data.warstrider.health.lethal + data.warstrider.health.aggravated) > totalWarstriderHealth) {
+    for (let [key, health_level] of Object.entries(actorData.data.data.warstrider.health.levels)) {
+      if ((actorData.data.data.warstrider.health.bashing + actorData.data.data.warstrider.health.lethal + actorData.data.data.warstrider.health.aggravated) > totalWarstriderHealth) {
         currentWarstriderPenalty = health_level.penalty;
       }
       totalWarstriderHealth += health_level.value;
     }
-    data.warstrider.health.total = totalWarstriderHealth;
-    if ((data.warstrider.health.bashing + data.warstrider.health.lethal + data.warstrider.health.aggravated) > data.warstrider.health.total) {
-      data.warstrider.health.aggravated = data.warstrider.health.total - data.warstrider.health.lethal;
-      if (data.warstrider.health.aggravated <= 0) {
-        data.warstrider.health.aggravated = 0;
-        data.warstrider.health.lethal = data.health.total;
+    actorData.data.data.warstrider.health.total = totalWarstriderHealth;
+    if ((actorData.data.data.warstrider.health.bashing + actorData.data.data.warstrider.health.lethal + actorData.data.data.warstrider.health.aggravated) > actorData.data.data.warstrider.health.total) {
+      actorData.data.warstrider.health.aggravated = actorData.data.warstrider.health.total - actorData.data.warstrider.health.lethal;
+      if (actorData.data.data.warstrider.health.aggravated <= 0) {
+        actorData.data.data.warstrider.health.aggravated = 0;
+        actorData.data.data.warstrider.health.lethal = actorData.data.data.health.total;
       }
     }
-    data.warstrider.health.penalty = currentWarstriderPenalty;
+    actorData.data.data.warstrider.health.penalty = currentWarstriderPenalty;
 
     
-    for (let [key, health_level] of Object.entries(data.ship.health.levels)) {
-      if ((data.ship.health.bashing + data.ship.health.lethal + data.ship.health.aggravated) > totalShipHealth) {
+    for (let [key, health_level] of Object.entries(actorData.data.data.ship.health.levels)) {
+      if ((actorData.data.data.ship.health.bashing + actorData.data.data.ship.health.lethal + actorData.data.data.ship.health.aggravated) > totalShipHealth) {
         currentShipPenalty = health_level.penalty;
       }
       totalShipHealth += health_level.value;
     }
-    data.ship.health.total = totalShipHealth;
-    if ((data.ship.health.bashing + data.ship.health.lethal + data.ship.health.aggravated) > data.ship.health.total) {
-      data.ship.health.aggravated = data.ship.health.total - data.ship.health.lethal;
-      if (data.ship.health.aggravated <= 0) {
-        data.ship.health.aggravated = 0;
-        data.ship.health.lethal = data.health.total;
+    actorData.data.data.ship.health.total = totalShipHealth;
+    if ((actorData.data.data.ship.health.bashing + actorData.data.data.ship.health.lethal + actorData.data.data.ship.health.aggravated) > actorData.data.data.ship.health.total) {
+      actorData.data.data.ship.health.aggravated = actorData.data.data.ship.health.total - actorData.data.data.ship.health.lethal;
+      if (actorData.data.data.ship.health.aggravated <= 0) {
+        actorData.data.data.ship.health.aggravated = 0;
+        actorData.data.data.ship.health.lethal = actorData.data.data.health.total;
       }
     }
-    data.ship.health.penalty = currentShipPenalty;
+    actorData.data.data.ship.health.penalty = currentShipPenalty;
 
     if (actorData.type !== "npc") {
-      data.experience.standard.spent = data.experience.standard.total - data.experience.standard.value;
-      data.experience.exalt.spent = data.experience.exalt.total - data.experience.exalt.value;
+      actorData.data.data.experience.standard.spent = actorData.data.data.experience.standard.total - actorData.data.data.experience.standard.value;
+      actorData.data.data.experience.exalt.spent = actorData.data.data.experience.exalt.total - actorData.data.data.experience.exalt.value;
     }
+
+    // Initialize containers.
+    const gear = [];
+    const weapons = [];
+    const armor = [];
+    const merits = [];
+    const intimacies = [];
+    const initiations = [];
+    const martialarts = [];
+    const crafts = [];
+    const specialties = [];
+    const specialAbilities = [];
+    const craftProjects = [];
+    const actions = [];
+
+
+    const charms = {
+      strength: { name: 'Ex3.Strength', visible: false, list: [] },
+      dexterity: { name: 'Ex3.Dexterity', visible: false, list: [] },
+      stamina: { name: 'Ex3.Stamina', visible: false, list: [] },
+      charisma: { name: 'Ex3.Charisma', visible: false, list: [] },
+      manipulation: { name: 'Ex3.Manipulation', visible: false, list: [] },
+      appearance: { name: 'Ex3.Appearance', visible: false, list: [] },
+      perception: { name: 'Ex3.Perception', visible: false, list: [] },
+      intelligence: { name: 'Ex3.Intelligence', visible: false, list: [] },
+      wits: { name: 'Ex3.Wits', visible: false, list: [] },
+      archery: { name: 'Ex3.Archery', visible: false, list: [] },
+      athletics: { name: 'Ex3.Athletics', visible: false, list: [] },
+      awareness: { name: 'Ex3.Awareness', visible: false, list: [] },
+      brawl: { name: 'Ex3.Brawl', visible: false, list: [] },
+      bureaucracy: { name: 'Ex3.Bureaucracy', visible: false, list: [] },
+      craft: { name: 'Ex3.Craft', visible: false, list: [] },
+      dodge: { name: 'Ex3.Dodge', visible: false, list: [] },
+      integrity: { name: 'Ex3.Integrity', visible: false, list: [] },
+      investigation: { name: 'Ex3.Investigation', visible: false, list: [] },
+      larceny: { name: 'Ex3.Larceny', visible: false, list: [] },
+      linguistics: { name: 'Ex3.Linguistics', visible: false, list: [] },
+      lore: { name: 'Ex3.Lore', visible: false, list: [] },
+      martialarts: { name: 'Ex3.MartialArts', visible: false, list: [] },
+      medicine: { name: 'Ex3.Medicine', visible: false, list: [] },
+      melee: { name: 'Ex3.Melee', visible: false, list: [] },
+      occult: { name: 'Ex3.Occult', visible: false, list: [] },
+      performance: { name: 'Ex3.Performance', visible: false, list: [] },
+      presence: { name: 'Ex3.Presence', visible: false, list: [] },
+      resistance: { name: 'Ex3.Resistance', visible: false, list: [] },
+      ride: { name: 'Ex3.Ride', visible: false, list: [] },
+      sail: { name: 'Ex3.Sail', visible: false, list: [] },
+      socialize: { name: 'Ex3.Socialize', visible: false, list: [] },
+      stealth: { name: 'Ex3.Stealth', visible: false, list: [] },
+      survival: { name: 'Ex3.Survival', visible: false, list: [] },
+      thrown: { name: 'Ex3.Thrown', visible: false, list: [] },
+      war: { name: 'Ex3.War', visible: false, list: [] },
+      evocation: { name: 'Ex3.Evocation', visible: false, list: [] },
+      other: { name: 'Ex3.Other', visible: false, list: [] },
+      universal: { name: 'Ex3.Universal', visible: false, list: [] },
+    }
+
+    const spells = {
+      terrestrial: { name: 'Ex3.Terrestrial', visible: false, list: [] },
+      celestial: { name: 'Ex3.Celestial', visible: false, list: [] },
+      solar: { name: 'Ex3.Solar', visible: false, list: [] },
+    }
+
+    // Iterate through items, allocating to containers
+    for (let i of actorData.items) {
+      let item = i.data;
+
+      item.img = i.img || DEFAULT_TOKEN;
+      if (i.type === 'item') {
+        gear.push(i);
+      }
+      else if (i.type === 'weapon') {
+        weapons.push(i);
+      }
+      else if (i.type === 'armor') {
+        armor.push(i);
+      }
+      else if (i.type === 'merit') {
+        merits.push(i);
+      }
+      else if (i.type === 'intimacy') {
+        intimacies.push(i);
+      }
+      else if (i.type === 'martialart') {
+        martialarts.push(i);
+      }
+      else if (i.type === 'craft') {
+        crafts.push(i);
+      }
+      else if (i.type === 'initiation') {
+        initiations.push(i);
+      }
+      else if (i.type === 'specialty') {
+        specialties.push(i);
+      }
+      else if (i.type === 'specialability') {
+        specialAbilities.push(i);
+      }
+      else if (i.type === 'craftproject') {
+        craftProjects.push(i);
+      }
+      else if (i.type === 'charm') {
+        if (i.data.ability !== undefined) {
+          charms[i.data.ability].list.push(i);
+          charms[i.data.ability].visible = true;
+        }
+      }
+      else if (i.type === 'spell') {
+        if (i.data.circle !== undefined) {
+          spells[i.data.circle].list.push(i);
+          spells[i.data.circle].visible = true;
+        }
+      }
+      else if (i.type === 'action') {
+        actions.push(i);
+      }
+    }
+
+    // Assign and return
+    actorData.gear = gear;
+    actorData.weapons = weapons;
+    actorData.armor = armor;
+    actorData.merits = merits;
+    actorData.martialarts = martialarts;
+    actorData.crafts = crafts;
+    actorData.initiations = initiations;
+    actorData.intimacies = intimacies;
+    actorData.specialties = specialties;
+    actorData.charms = charms;
+    actorData.spells = spells;
+    actorData.specialabilities = specialAbilities;
+    actorData.projects = craftProjects;
+    actorData.actions = actions;
+
+    console.log(actorData);
   }
 }
