54d8da176d6a9460031210c91f2aa0fc6dd22f2c
Aliharu <christopherdron@gmail.com>
2023-01-30 17:47:45 -0700

    weapon/armor weight type, cost, charm/spell summary

--- module/item/item.js
+++ module/item/item.js
@@ -17,6 +17,128 @@ export class ExaltedThirdItem extends Item {
     }
   }
 
+  async _preUpdate(updateData, options, user) {
+    await super._preUpdate(updateData, options, user);
+    if (updateData.system?.weighttype && updateData.system?.weighttype !== 'other' && (!this.actor || this.actor.type === 'character')) {
+      const equipmentChart = {
+        light: {
+          accuracy: 4,
+          damage: 7,
+          defense: 0,
+          overwhelming: 1,
+          attunement: 5,
+          soak: 3,
+          hardness: 0,
+          penalty: 0,
+        },
+        medium: {
+          accuracy: 2,
+          damage: 9,
+          defense: 1,
+          overwhelming: 1,
+          attunement: 5,
+          soak: 5,
+          hardness: 0,
+          penalty: 1,
+        },
+        heavy: {
+          accuracy: 0,
+          damage: 11,
+          defense: -1,
+          overwhelming: 1,
+          attunement: 5,
+          soak: 7,
+          hardness: 0,
+          penalty: 2,
+        },
+        siege: {
+          accuracy: -3,
+          damage: 15,
+          defense: 0,
+          overwhelming: 3,
+          attunement: 5,
+        },
+      };
+      const artifactEquipmentChart = {
+        light: {
+          accuracy: 5,
+          damage: 10,
+          defense: 0,
+          overwhelming: 3,
+          attunement: 5,
+          soak: 5,
+          hardness: 4,
+          penalty: 0,
+        },
+        medium: {
+          accuracy: 3,
+          damage: 12,
+          defense: 1,
+          overwhelming: 4,
+          attunement: 5,
+          soak: 8,
+          hardness: 7,
+          penalty: 1,
+        },
+        heavy: {
+          accuracy: 1,
+          damage: 14,
+          defense: 0,
+          overwhelming: 5,
+          attunement: 5,
+          soak: 11,
+          hardness: 10,
+          penalty: 2,
+        },
+        siege: {
+          accuracy: -2,
+          damage: 20,
+          defense: 0,
+          overwhelming: 5,
+          attunement: 5,
+        },
+      };
+      if (this.type === 'weapon') {
+        if (this.system.traits.weapontags?.value?.includes('artifact')) {
+          updateData.system.witheringaccuracy = artifactEquipmentChart[updateData.system?.weighttype].accuracy;
+          updateData.system.witheringdamage = artifactEquipmentChart[updateData.system?.weighttype].damage;
+          updateData.system.defense = artifactEquipmentChart[updateData.system?.weighttype].defense;
+          updateData.system.overwhelming = artifactEquipmentChart[updateData.system?.weighttype].overwhelming;
+          updateData.system.attunement = artifactEquipmentChart[updateData.system?.weighttype].attunement;
+        }
+        else {
+          updateData.system.witheringaccuracy = equipmentChart[updateData.system?.weighttype].accuracy;
+          updateData.system.witheringdamage = equipmentChart[updateData.system?.weighttype].damage;
+          updateData.system.defense = equipmentChart[updateData.system?.weighttype].defense;
+          updateData.system.overwhelming = equipmentChart[updateData.system?.weighttype].overwhelming;
+          updateData.system.attunement = 0;
+        }
+      }
+      if (this.type === 'armor') {
+        if(this.system.traits.armortags?.value?.includes('artifact')) {
+          if(updateData.system?.weighttype === 'light'){
+            updateData.system.attunement = 4;
+          }
+          else if(updateData.system?.weighttype === 'heavy') {
+            updateData.system.attunement = 6;
+          }
+          else {
+            updateData.system.attunement = 5;
+          }
+          updateData.system.soak = artifactEquipmentChart[updateData.system?.weighttype].soak;
+          updateData.system.hardness = artifactEquipmentChart[updateData.system?.weighttype].hardness;
+          updateData.system.penalty = artifactEquipmentChart[updateData.system?.weighttype].penalty;
+        }
+        else {
+          updateData.system.soak = equipmentChart[updateData.system?.weighttype].soak;
+          updateData.system.hardness = equipmentChart[updateData.system?.weighttype].hardness;
+          updateData.system.penalty = equipmentChart[updateData.system?.weighttype].penalty;
+          updateData.system.attunement = 0;
+        }
+      }
+    }
+  }
+
   getImageUrl(type) {
     if (type === 'intimacy') {
       return "systems/exaltedthird/assets/icons/hearts.svg";
